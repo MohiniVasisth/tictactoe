1834a37b9ed532098bd679d14cf6048e95563dc6
Mohini <mohinivasisth4@gmail.com>
2022-03-09 22:13:22 +0530

    init comit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+      <p>My instructor name is Andrew sir.</p>
+    </>
+  );
+};
+
+export default App;
